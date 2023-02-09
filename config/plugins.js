d8def0c8f5ff09f041ccea1ec09474864296d7b9
David Obermair <46714163+Obermair@users.noreply.github.com>
2023-02-09 18:17:57 +0100

    changed env

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
     email: {
       provider: 'sendgrid',
       providerOptions: {
-        apiKey: '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',
+        apiKey: env('SENDGRID_API_KEY'),
       },
       settings: {
         defaultFrom: 'jobquadrat@gmail.com',
